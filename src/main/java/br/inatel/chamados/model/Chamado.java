765424956a321a18eb727660296329ee44c55b07
Pedro Henrique dos Santos Araujo <phsa2004@gmail.com>
2026-09-07 17:42:37 -0300

    feat: cria entidade Chamado

---
+++ src/main/java/br/inatel/chamados/model/Chamado.java
@@ -0,0 +1,83 @@
+package br.inatel.chamados.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "chamado")
+public class Chamado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Prioridade prioridade;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusChamado status;
+
+    @Column(nullable = false)
+    private LocalDateTime dataAbertura;
+
+    public Chamado() {
+    }
+
+    public Chamado(String titulo, String descricao, Prioridade prioridade) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.prioridade = prioridade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Prioridade getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(Prioridade prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public StatusChamado getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusChamado status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(LocalDateTime dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+}
